b2886a5fda64fe8e88d4be608b958f56f1693eae
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-03-23 04:13:34 -0300

    Finalizando script com medidor de desempenho
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- Lista_1/Lista_1.py
+++ Lista_1/Lista_1.py
@@ -1,58 +1,90 @@
 import numpy as np
 import sys
+import ipdb
+import time
 
+# parametros do algoritmo
+quantidade_numeros = 100000
+limite_inferior = 0
+limite_superior = quantidade_numeros*10
+gap_percentage = 10 #max value for this is 100
 
-# Gera valores aleatorios
-def valor_aleatorio():
-    valor = np.random.randint(0, 1000)
-    return valor
+lista_de_valores = list(
+    np.linspace(limite_inferior, limite_superior, num=quantidade_numeros, endpoint=False, dtype=int)
+    )
 
-# Gera um vetor de tamanho 1001  com valores inteiros aleatorios > 10000
-lista_de_valores = np.linspace(0, 10000, num=101, endpoint=False, dtype=int)
-print (lista_de_valores)
+vetor_de_indice = [i*gap_percentage for i in range(quantidade_numeros//gap_percentage)]
+vetor_de_indice.append(quantidade_numeros-1)
 
-# Converte a lista de valores aleatorios para array numpy
-vetor = np.asarray(lista_de_valores)
+def busca_binaria(lista, valor):
+    posicao = -1
+    if lista:
+        l = 0
+        r = len(lista)-1
+        mid = (l+r)//2
+        if(l<r):
+            if(lista[mid] == valor):
+                posicao = mid
+            elif (lista[mid] < valor):
+                posicao = mid + 1 + busca_binaria(lista[mid+1:], valor)
+            else:
+                posicao = l + busca_binaria(lista[:mid], valor)
+        elif lista[0] == valor:
+            posicao = 0
+    return posicao
 
-# Gera uma lista de valores para servir como base para o intervalo do vetor_de_index
-lista_de_index = []
-for index in range(101):
-    lista_de_index.append(index)
+# Busca sequencial no indice da lista 
+def busca_no_indice(indice, lista, valor):
+    indice_fim = len(indice)-1
+    encontrou_maior = False #flag
+    posicao = -1
+    
+    if indice and lista:
+        if lista[indice[0]] <= valor and lista[indice[indice_fim]] > valor:
+            for i in range (indice_fim+1):
+                if (not encontrou_maior) and lista[indice[i]] > valor:
+                    posicao_relativa = busca_binaria(lista[indice[(i-1)]:indice[i]], valor)
+                    if not(posicao_relativa == -1):
+                        posicao = indice[i-1] + posicao_relativa
+                    encontrou_maior = True
+        elif lista[indice[indice_fim]] == valor:
+            posicao = indice[indice_fim]
 
-# Cria o vetor de index com um intervalo de 10
-vetor_de_index = [index for index in lista_de_index if index % 10 == 0]
-print (vetor_de_index)
-
-# Faz a indexacao do vetor
-print(vetor[np.array(vetor_de_index)])
-
-
-def busca_binaria(array, valor):
-    l = 0
-    r = len(array)-1
-    mid = (l+r)/2
-
-    while(l < r):
-        if(array[mid] == valor):
-            return mid
-        elif (array[mid] < valor):
-            return mid + 1 + busca_binaria(array[mid+1:], valor)
-        else:
-            return busca_binaria(array[:mid], valor)
-
-    if (array[mid] != valor):
-        print "valor nao encontrado"
-        return -1
-
-    return mid
+    return posicao
 
 
 def main():
     n = input('Digite o valor que deseja procurar no vetor: \n')
+    start = time.time()
     value = int(n)
+    posicao = busca_no_indice(vetor_de_indice, lista_de_valores, value)
+    if posicao >= 0:
+        pass
+        print "O valor esta localizado na posicao: ", posicao
+    else:
+        pass
+        print "valor nao encontrado"
 
-    print "O valor esta localizado na posicao: ", busca_binaria(vetor_de_index, value)
+    end = time.time()
+    print(end - start)
 
+def performance():
+    start = time.time()
+    for n in lista_de_valores:
+        value = int(n)
+        posicao = busca_no_indice(vetor_de_indice, lista_de_valores, value)
+        if posicao >= 0:
+            pass
+        else:
+            pass
+
+    end = time.time()
+    print("Tempo de resposta: ", end - start)
 
 if __name__ == '__main__':
-    main()
+    n = int(input('0 - Performance Automatica\n1 - Busca Manual: \n'))
+    
+    if n:
+        main()
+    else:
+        performance()
